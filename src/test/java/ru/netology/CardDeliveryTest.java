cb6c30c0c494a2cc58b112300c6c48a9a82e1254
Elza <barywn9@yandex.ru>
2026-06-09 18:47:34 +0300

    Initial commit

---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    void shouldCardDeliveryForm() throws InterruptedException {
+        Selenide.open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Элиста");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
+        String planningDate = generateDate(4);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Сидоров Иван");
+        $("[data-test-id='phone'] input").setValue("+79270000001");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $("[data-test-id='notification']")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Успешно!"));
+    }
+}
